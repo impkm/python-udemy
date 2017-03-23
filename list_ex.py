431dc4f10a34a685240df61985028b2afc0570c9
paul martinez <Paul@pauls-MacBook-Air-2.local>
2017-03-23 10:54:17 -0700

    list_ex.py

---
+++ list_ex.py
@@ -0,0 +1,16 @@
+my_list = [12, 334, 34, 54, 54, 32] #my first list
+print(my_list) #prints my first list
+
+my_list.pop() #removes last item in list
+print(my_list) #prints new list
+
+my_list.remove(12) #removes what is called.
+print(my_list) #prints my list
+
+
+
+
+
+
+
+
