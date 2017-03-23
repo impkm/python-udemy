f9dd82e7a65a44cf57cd7376cdca95eea56209e6
paulkm28 <paul.k.martinez@gmail.com>
2017-03-23 11:00:22 -0700

    Add files via upload

---
+++ list_ex.py
@@ -0,0 +1,17 @@
+my_list = [12, 334, 34, 54, 54, 32] #my first list
+print(my_list) #prints my first list
+
+my_list.pop() #removes last item in list
+print(my_list) #prints new list
+
+my_list.remove(12) #removes what is called.
+print(my_list) #prints my list
+
+len(my_list)
+print(len(my_list))
+
+
+
+
+
+
