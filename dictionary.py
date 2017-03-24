49df7c00957c7cfd41880f02d8b007e883d5c776
Paul <paul.k.martinez@gmail.com>
2017-03-24 09:40:09 -0700

    inceptions for keys

--- dictionary.py
+++ dictionary.py
@@ -17,3 +17,16 @@ print(dict2) # prints new dictionary
 
 print(dict2.keys()) # prints new dictionary keys
 
+dict1 = {'k1': 23, 'k2': 33, 'k3':'paul'}
+print(dict1['k1']) # prints value in 1st key
+print(dict1.keys()) # prints keys from dictionary
+print(dict1.values()) # prints values from keys
+dict1['k1'] += 100 # adds 100 to k1
+print(dict1['k1']) # prints k1 new value
+print(dict1) # prints
+
+dict3 = {'inception': {'leo': {'titanic': {'rose'}}}} # dictionary inception
+print(dict3) # prints dictionary
+print(dict3['inception'] ['leo'] ['titanic']) # prints value to 3rd dictionary
+
+
