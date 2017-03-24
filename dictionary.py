5810e09fc4800bbe9a381ae080da299fe2e69029
Paul <paul.k.martinez@gmail.com>
2017-03-24 09:21:34 -0700

    added new dict

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,19 @@
-my_dict = {'username':'1123223', 'Password':'12334321'} #my first dictionary
-my_dict['username'] #gives value to first key
+my_dict = {'username':'paul', 'Password':'12334321'} # my first dictionary
+#my_dict['username'] # gives value to first key
 
-print(my_dict['username']) #prints value to username
-print(my_dict['Password']) #prints value to password
-print(my_dict.keys()) #prints keys to dictionary
+print(my_dict['username']) # prints value to username
+print(my_dict['Password']) # prints value to password
+print(my_dict.keys()) # prints keys to dictionary
 
+my_dict.clear() # clear
+print(my_dict) # prints brackets
 
+dict2 = {} # creates new dictionary
+print(dict2) # prints empty dictionary
+
+dict2['jon'] = 43 # adds to dictionary
+dict2['will'] = 32 # adds to dictionary
+print(dict2) # prints new dictionary
+
+print(dict2.keys()) # prints new dictionary keys
 
