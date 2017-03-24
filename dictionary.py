b70ed06e11ec25397a5d4ea0b94c33f05d135015
Paul <paul.k.martinez@gmail.com>
2017-03-23 19:25:40 -0700

    first commit for dictionary

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+my_dict = {'username':'1123223', 'Password':'12334321'} #my first dictionary
+my_dict['username'] #gives value to first key
+
+print(my_dict['username']) #prints value to username
+print(my_dict['Password']) #prints value to password
+print(my_dict.keys()) #prints keys to dictionary
+
+
+
