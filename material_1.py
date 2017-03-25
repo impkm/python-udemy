59c70ba2a2398d465d853be332324b1009c583de
Paul <paul.k.martinez@gmail.com>
2017-03-25 08:34:16 -0700

    material 1 with lists

--- material_1.py
+++ material_1.py
@@ -1,20 +1,37 @@
 # Write an equation that uses multiplication, division, an exponent,
 # addition, and subtraction that is equal to 100.25
 
-x1 = 401.0/4
+x1 = 401.0/4  #division
 print(x1)
-x2 = 20.05 * 5
+x2 = 20.05 * 5  #multiplication
 print(x2)
-x3 = 100 + 0.25
+x3 = 100 + 0.25  #addition
 print(x3)
-x4 = 200 - 99.75
+x4 = 200 - 99.75  #subrtaction
 print(x4)
 
-print('What is the value of the expression 4 * (6 + 5)')
-print('The value is: {}' .format(4 * (6 + 5)))
+#print statements with arithmetic
 
-print('What is the value of the expression 4 * 6 + 5')
-print('The Value is: {}'.format(4*6+5))
+print('What is the value of the expression 4 * (6 + 5)')  #lets print stuff
+print('The value is: {}' .format(4 * (6 + 5)))  #lets print stuff
 
-print('What is the value of the expression 4 + 6 * 5')
-print('The value is: '.format(4+6*5))
+print('What is the value of the expression 4 * 6 + 5')  #lets print stuff
+print('The Value is: {}'.format(4*6+5))  #lets print stuff
+
+print('What is the value of the expression 4 + 6 * 5')  #lets print stuff
+print('The value is: {}'.format(4+6*5))  #lets print stuff
+
+#printing with strings
+string1 = 'hello'
+print(string1[1])  # prints what is located in locatoin '1'
+print(string1[::-1])  # prints hello in reverse
+print(string1[-1])  # prints the call from the end -1
+print(string1[4])  # prints what is located in location 4
+
+#build list two different ways
+list1 = [1]*3  # creates list of a same number * 3
+list2 = [1, 2, 3]  # basic method for creating lists
+print(list1, list2)
+
+
+
