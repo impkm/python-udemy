29ad2ca228781fed498febd2af54b1c13c9669f2
Paul <paul.k.martinez@gmail.com>
2017-03-25 07:49:51 -0700

    first commit

---
+++ material_1.py
@@ -0,0 +1,20 @@
+# Write an equation that uses multiplication, division, an exponent,
+# addition, and subtraction that is equal to 100.25
+
+x1 = 401.0/4
+print(x1)
+x2 = 20.05 * 5
+print(x2)
+x3 = 100 + 0.25
+print(x3)
+x4 = 200 - 99.75
+print(x4)
+
+print('What is the value of the expression 4 * (6 + 5)')
+print('The value is: {}' .format(4 * (6 + 5)))
+
+print('What is the value of the expression 4 * 6 + 5')
+print('The Value is: {}'.format(4*6+5))
+
+print('What is the value of the expression 4 + 6 * 5')
+print('The value is: '.format(4+6*5))
